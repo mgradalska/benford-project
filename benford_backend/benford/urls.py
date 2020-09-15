3661f3f5b0a7ee5544b4b32ce6a972397d6d05e8
mgradalska <martagradalska@gmail.com>
2020-09-15 22:16:46 +0200

    Remove admin link, refactor backend tests

--- benford_backend/benford/urls.py
+++ benford_backend/benford/urls.py
@@ -1,5 +1,3 @@
-from django.contrib import admin
-from django.urls import path
 from rest_framework import routers
 from benford_analyzer.views import BenfordAnalyzerView
 from django.conf.urls import include, url
@@ -10,7 +8,6 @@ router = routers.DefaultRouter()
 router.register(r"datasets", BenfordAnalyzerView, basename="benford analyzer")
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     url(r"api/", include(router.urls)),
 ]
 
--- benford_backend/benford_analyzer/tests.py
+++ benford_backend/benford_analyzer/tests.py
@@ -1,7 +1,6 @@
 import os
 import tempfile
 
-from django.core.files import File
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase, override_settings
@@ -21,60 +20,67 @@ files_path = os.path.join(settings.BASE_DIR, "benford_analyzer/test_data/")
 class DatasetTestCase(TestCase):
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_correct_file_is_analyzed(self):
-        file_content = File(open(f"{files_path}correct")).read()
-        mock_file = SimpleUploadedFile("correct", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        dataset.calculate_benford_distribution()
-        assert dataset.distribution_match == True
-        assert dataset.graph is not None
-        assert dataset.statistics.count() == 9
+        with open(f"{files_path}correct", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("correct", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            dataset.calculate_benford_distribution()
+            assert dataset.distribution_match == True
+            assert dataset.graph is not None
+            assert dataset.statistics.count() == 9
 
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_not_benford_file_is_analyzed(self):
-        file_content = File(open(f"{files_path}not_benford_data")).read()
-        mock_file = SimpleUploadedFile("not_benford_data", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        dataset.calculate_benford_distribution()
-        assert dataset.distribution_match == False
-        assert dataset.graph is not None
-        assert dataset.statistics.count() == 9
+        with open(f"{files_path}not_benford_data", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("not_benford_data", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            dataset.calculate_benford_distribution()
+            assert dataset.distribution_match == False
+            assert dataset.graph is not None
+            assert dataset.statistics.count() == 9
 
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_analyze_failes_when_column_is_missing(self):
-        file_content = File(open(f"{files_path}missing_column")).read()
-        mock_file = SimpleUploadedFile("missing_column", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        with self.assertRaises(IncorrectFileStructureException):
-            dataset.calculate_benford_distribution()
+        with open(f"{files_path}missing_column", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("missing_column", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            with self.assertRaises(IncorrectFileStructureException):
+                dataset.calculate_benford_distribution()
 
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_analyze_failes_when_data_not_integers(self):
-        file_content = File(open(f"{files_path}incorrect_data")).read()
-        mock_file = SimpleUploadedFile("icorrect_data", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        with self.assertRaises(IncorrectDataException):
-            dataset.calculate_benford_distribution()
+        with open(f"{files_path}incorrect_data", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("icorrect_data", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            with self.assertRaises(IncorrectDataException):
+                dataset.calculate_benford_distribution()
 
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_analyze_failes_when_some_data_is_empty(self):
-        file_content = File(open(f"{files_path}empty_data")).read()
-        mock_file = SimpleUploadedFile("empty_data", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        with self.assertRaises(IncorrectDataException):
-            dataset.calculate_benford_distribution()
+        with open(f"{files_path}empty_data", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("empty_data", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            with self.assertRaises(IncorrectDataException):
+                dataset.calculate_benford_distribution()
 
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_analyze_failes_when_given_empty_file(self):
-        file_content = File(open(f"{files_path}empty_file")).read()
-        mock_file = SimpleUploadedFile("empty_file", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        with self.assertRaises(EmptyFileException):
-            dataset.calculate_benford_distribution()
+        with open(f"{files_path}empty_file", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("empty_file", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            with self.assertRaises(EmptyFileException):
+                dataset.calculate_benford_distribution()
 
     @override_settings(MEDIA_ROOT=tempfile.TemporaryDirectory().name)
     def test_analyze_failes_when_given_incorrect_file(self):
-        file_content = File(open(f"{files_path}incorrect_file.png")).read()
-        mock_file = SimpleUploadedFile("incorrect_file", bytes(file_content, "utf-8"))
-        dataset = Dataset.objects.create(file=mock_file)
-        with self.assertRaises(IncorrectFileException):
-            dataset.calculate_benford_distribution()
+        with open(f"{files_path}incorrect_file.png", "rb") as f:
+            file_content = f.read()
+            mock_file = SimpleUploadedFile("incorrect_file", file_content)
+            dataset = Dataset.objects.create(file=mock_file)
+            with self.assertRaises(IncorrectFileException):
+                dataset.calculate_benford_distribution()
